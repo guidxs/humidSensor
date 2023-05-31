996e55a64b80c994d69e10316b17e8d91c4a7117
Guilherme Doretto Sobreiro <126616600+GuiDoretto@users.noreply.github.com>
2023-05-30 22:06:49 -0300

    Update humidade.c

--- humidade.c
+++ humidade.c
@@ -1,4 +1,5 @@
 int umidade = 0;
+int pinoBuzzer = 13;
 
 void setup()
 {
@@ -10,39 +11,40 @@ void setup()
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
   pinMode(12, OUTPUT);
+  pinMode(pinoBuzzer, OUTPUT); // Configura o pino do buzzer como saída
 }
 
 void loop()
 {
-  // Aplica alimentação ao sensor de umidade do solo
+  // Aplica energia ao sensor de umidade do solo
   digitalWrite(A0, HIGH);
   delay(10); // Aguarda 10 milissegundos
   umidade = analogRead(A1);
-  // Desliga o sensor para reduzir a corrosão dos metais
+  // Desliga o sensor para reduzir a corrosão do metal
   // ao longo do tempo
   digitalWrite(A0, LOW);
   Serial.println(umidade);
+
+  // Desliga todos os LEDs e o buzzer
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
   digitalWrite(12, LOW);
+  digitalWrite(pinoBuzzer, LOW);
+
   if (umidade < 200) {
     digitalWrite(12, HIGH);
+    digitalWrite(pinoBuzzer, HIGH); // Liga o buzzer
+  } else if (umidade < 400) {
+    digitalWrite(11, HIGH);
+  } else if (umidade < 600) {
+    digitalWrite(10, HIGH);
+  } else if (umidade < 800) {
+    digitalWrite(9, HIGH);
   } else {
-    if (umidade < 400) {
-      digitalWrite(11, HIGH);
-    } else {
-      if (umidade < 600) {
-        digitalWrite(10, HIGH);
-      } else {
-        if (umidade < 800) {
-          digitalWrite(9, HIGH);
-        } else {
-          digitalWrite(8, HIGH);
-        }
-      }
-    }
+    digitalWrite(8, HIGH);
   }
+
   delay(100); // Aguarda 100 milissegundos
 }
